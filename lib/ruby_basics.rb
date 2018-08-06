94b0ddcc11792c239a1a7c2d2a8a5405fa0a36d4
Brian Kang <brianhkang1@gmail.com>
2018-08-06 19:47:29 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,18 +10,17 @@ def argue(argument)
   argument
 end
 
-def greeting
-  
+def greeting(arg1, arg2)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg="cheese")
+  arg
 end
